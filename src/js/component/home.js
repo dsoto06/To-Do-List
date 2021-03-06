38b5d9b901b11f96f0877af1d76f52b686850bed
dsoto06 <diegosr06@hotmail.com>
2021-03-06 06:33:41 +0000

    entrega

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,60 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	let [task, setTask] = useState("");
+	const [taskLists, setTaskLists] = useState([]);
+	const change = e => {
+		setTask(e.target.value);
+	};
+	const AddTask = () => {
+		if (task !== "") {
+			const taskDetails = {
+				id: Math.floor(Math.random() * 1000),
+				value: task
+			};
+
+			setTaskLists([...taskLists, taskDetails]);
+		}
+	};
+	const deletetask = (e, id) => {
+		e.preventDefault();
+		setTaskLists(taskLists.filter(t => t.id != id));
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="title">
+				<h1>To Do List</h1>
+			</div>
+			<div className="list-group">
+				<ul className="list-group">
+					<li className="list-group-item">
+						<input
+							onChange={e => change(e)}
+							placeholder="New Item"></input>
+						<button
+							className="btn btn-success float-right"
+							onClick={AddTask}>
+							Add
+						</button>
+					</li>
+					{taskLists !== [] ? (
+						<li>
+							{taskLists.map(t => (
+								<li className="list-group-item">
+									{t.value}
+									<button
+										className="btn btn-danger float-right"
+										onClick={e => deletetask(e, t.id)}>
+										X
+									</button>
+								</li>
+							))}
+						</li>
+					) : null}
+					<li className="list-group-item">contador</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
